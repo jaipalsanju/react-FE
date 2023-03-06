8589cf430de63a2c6f78a3a2eb73dbf9f68d60eb
jaipalsanju <118240902+jaipalsanju@users.noreply.github.com>
2023-03-06 09:35:27 +0530

    1 file modified

--- src/Table.js
+++ src/Table.js
@@ -3,24 +3,24 @@ import React from "react";
 export default function Table(){
   return(
     <table >
-        <tr>
+        <tbody>
             <th>Name</th>
             <th>class</th>
             <th>address</th>
             <th>contact</th>
-        </tr>
-        <tr>
+        </tbody>
+        <tbody>
             <td>ram</td>
             <td>9th</td>
             <td>bglr</td>
             <td>93927467</td>
-        </tr>
-        <tr>
+        </tbody>
+        <tbody>
             <td>prem</td>
             <td>8th</td>
             <td>hyd</td>
             <td>95969594</td>
-        </tr>
+        </tbody>
     </table>
   )
 }
